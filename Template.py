8067bc7c652bb360f12d283598682f0aaaaa18cc
Tsubasa <tsubasa.umeuchi@gmail.com>
2018-02-15 11:52:23 +0900

    [update] Update Template.py

--- Template.py
+++ Template.py
@@ -1,4 +1,42 @@
+import sys
+import math
+import collections
+import itertools
+import array
+import inspect
+
+# Set max recursion limit
+sys.setrecursionlimit(10000)
+
+# Debug output
+def chkprint(*args):
+    names = {id(v):k for k,v in inspect.currentframe().f_back.f_locals.items()}
+    print(', '.join(names.get(id(arg),'???')+' = '+repr(arg) for arg in args))
+
+# Binary converter
+def to_bin(x):
+    return bin(x)[2:]
+
+# Set 2 dimension list
+def dim2input(N):
+    li = []
+    for _ in range(N):
+        li.append(list(map(int, input())))
+    return li
+
+""" input template
+S = input()
+N = int(input())
+L = list(map(int, input().split()))
+a, b = list(map(int, input().split()))
+SL = list(input())
+"""
+
+# --------------------------------------------
+
+dp = None
+
 def main():
-	pass
+    pass
 
 main()
