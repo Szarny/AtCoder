9037050d99af7d49201e03e9c36faa74c999b2e6
Tsubasa Umeuchi <tsubasa.umeuchi@gmail.com>
2018-08-09 10:11:03 +0900

    Solved ABC085(D)

--- AtCoder_Beginner_Contest/085/4.py
+++ AtCoder_Beginner_Contest/085/4.py
@@ -1,31 +1,67 @@
+import sys
 import math
+import collections
+import itertools
+import array
+import inspect
+
+# Set max recursion limit
+sys.setrecursionlimit(10000)
+
+
+# Debug output
+def chkprint(*args):
+    names = {
+        id(v): k
+        for k, v in inspect.currentframe().f_back.f_locals.items()
+    }
+    print(', '.join(
+        names.get(id(arg), '???') + ' = ' + repr(arg) for arg in args))
+
+
+# Binary converter
+def to_bin(x):
+    return bin(x)[2:]
+
+
+def li_input():
+    return [int(_) for _ in input().split()]
+
+
+# --------------------------------------------
+
+dp = None
+
 
 def main():
-    N, H = map(int, input().split())
+    N, H = li_input()
+    S = [li_input() for _ in range(N)]
 
-    cuts = []
-    throws = []
-    for i in range(N):
-        tmp = [int(j) for j in input().split()]
-        cuts.append(tmp[0])
-        throws.append(tmp[1])
+    ans = 0
+    strong_throw = []
 
-    max_cut = max(cuts)
-    t1 = 0
+    maxcut = -1
+    for s in S:
+        if s[0] > maxcut:
+            maxcut = s[0]
 
-    throws.sort(reverse=True)
+    for s in S:
+        if s[1] > maxcut:
+            strong_throw.append(s[1])
 
-    for throw in throws:
-        if throw < max_cut:
+    strong_throw.sort(reverse=True)
+
+    for st in strong_throw:
+        H -= st
+        ans += 1
+
+        if H <= 0:
             break
-        H -= throw
-        t1 += 1
 
-    t2 = math.ceil(H / max_cut)
+    if H > 0:
+        ans += math.ceil(H / maxcut)
 
-    print(t1 + t2)
-
-
+    print(ans)
 
 
 main()
