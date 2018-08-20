bf615171d94639484807486a3b4049a163af1e48
Tsubasa Umeuchi <tsubasa.umeuchi@gmail.com>
2018-08-20 10:17:59 +0900

    テンプレートを更新

--- Template.py
+++ Template.py
@@ -8,23 +8,33 @@ import inspect
 # Set max recursion limit
 sys.setrecursionlimit(10000)
 
+
 # Debug output
 def chkprint(*args):
-    names = {id(v):k for k,v in inspect.currentframe().f_back.f_locals.items()}
-    print(', '.join(names.get(id(arg),'???')+' = '+repr(arg) for arg in args))
+    names = {
+        id(v): k
+        for k, v in inspect.currentframe().f_back.f_locals.items()
+    }
+    print(', '.join(
+        names.get(id(arg), '???') + ' = ' + repr(arg) for arg in args))
+
 
 # Binary converter
 def to_bin(x):
     return bin(x)[2:]
 
+
 def li_input():
     return [int(_) for _ in input().split()]
+
 
 # --------------------------------------------
 
 dp = None
 
+
 def main():
     pass
+
 
 main()
